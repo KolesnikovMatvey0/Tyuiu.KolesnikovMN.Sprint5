7bb9e1be8a718d4c52bd2eb7bf09222ac647b345
Kolesnikov Matvey <matveydev0@gmail.com>
2024-10-26 23:12:06 +0500

    Task0

--- Tyuiu.KolesnikovMN.Sprint5.Task0.V26.Lib/DataService.cs
+++ Tyuiu.KolesnikovMN.Sprint5.Task0.V26.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.KolesnikovMN.Sprint5.Task0.V26.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string[] paths = { $@"{Directory.GetCurrentDirectory() }", "OutPutFileTask0.txt" };
+            string[] paths = { $@"{Path.GetTempPath()}", "OutPutFileTask0.txt" };
             string fullPath = Path.Combine(paths);
             double z = Math.Round( 0.7 * Math.Pow( x, 3 ) + 1.52 * Math.Pow( x, 2 ), 3 );
             File.WriteAllText( fullPath, z.ToString() );
--- Tyuiu.KolesnikovMN.Sprint5.Task0.V26.Test/DataServiceTest.cs
+++ Tyuiu.KolesnikovMN.Sprint5.Task0.V26.Test/DataServiceTest.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.KolesnikovMN.Sprint5.Task0.V26.Test
         [TestMethod]
         public void CheckedExistsFile()
         {
-            string path = @"C:\Users\Matvey\source\repos\Tyuiu.KolesnikovMN.Sprint5\Tyuiu.KolesnikovMN.Sprint5.Task0.V26\bin\Debug\net8.0\OutPutFileTask0.txt";
+            string path = @"C:\Users\Matvey\AppData\Local\Temp\OutPutFileTask0.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             Assert.AreEqual(true, fileExists);
