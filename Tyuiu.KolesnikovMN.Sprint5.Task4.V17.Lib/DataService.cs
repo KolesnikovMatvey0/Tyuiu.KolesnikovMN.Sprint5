4b44ff1d694485f3a44bdb53fd6ab08a8b9b9c0c
Kolesnikov Matvey <matveydev0@gmail.com>
2024-10-31 19:52:15 +0500

    Task4

--- Tyuiu.KolesnikovMN.Sprint5.Task4.V17.Lib/DataService.cs
+++ Tyuiu.KolesnikovMN.Sprint5.Task4.V17.Lib/DataService.cs
@@ -6,16 +6,9 @@ namespace Tyuiu.KolesnikovMN.Sprint5.Task4.V17.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string str = File.ReadAllText(path);
+            string strX = File.ReadAllText(path);
 
-            string strX = "";
-            foreach ( string ch in str.Split() )
-            {
-                if (ch == ",")
-                    strX += ".";
-                else
-                    strX += ch;
-            }
+            strX = strX.Replace('.', ',');
             double x = Convert.ToDouble(strX);
 
             double res = Math.Round( Math.Sin( 2 / ( 3 * x ) ) + Math.Pow( x, 2 ), 3 );
