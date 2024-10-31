c1b09b5eb143e7c3def99156c5cab58f0b226879
Kolesnikov Matvey <matveydev0@gmail.com>
2024-10-31 20:58:28 +0500

    Task5

--- Tyuiu.KolesnikovMN.Sprint5.Task5.V9.Lib/DataService.cs
+++ Tyuiu.KolesnikovMN.Sprint5.Task5.V9.Lib/DataService.cs
@@ -6,27 +6,17 @@ namespace Tyuiu.KolesnikovMN.Sprint5.Task5.V9.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            double res = double.MaxValue;
+            double res = double.MinValue;
             using ( StreamReader reader = new StreamReader(path) )
             {
-                //string line;
-                //while ( ( line = reader.ReadLine() ) != null)
-                //{
-                //    line = line.Replace('.', ',');
-                //    if ( res < Convert.ToDouble( line ) )
-                //        res = Convert.ToDouble( line );
-                //}
-                string line = reader.ReadLine();
-                string[] nums = line.Split();
-                for ( int i = 0; i < nums.Length; i++)
+                string line;
+                while ( ( line = reader.ReadLine() ) != null)
                 {
-                    nums[i] = nums[i].Replace('.', ',');
-                    if ( nums[i][nums[i].Length-1] == '0')
-                    {
-                        if (res < Convert.ToDouble(nums[i]))
-                            res = Convert.ToDouble(nums[i]);
-                    }
+                    line = line.Replace('.', ',');
+                    if ( res < Convert.ToDouble( line ) )
+                        res = Convert.ToDouble( line );
                 }
+
             }
             return res;
         }
