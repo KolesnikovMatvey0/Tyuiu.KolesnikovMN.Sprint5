9168d7ac114a23689e314b1561e86235d466b3f6
Kolesnikov Matvey <matveydev0@gmail.com>
2024-12-22 21:23:53 +0500

    Task5

--- Tyuiu.KolesnikovMN.Sprint5.Task5.V9.Lib/DataService.cs
+++ Tyuiu.KolesnikovMN.Sprint5.Task5.V9.Lib/DataService.cs
@@ -4,6 +4,8 @@ namespace Tyuiu.KolesnikovMN.Sprint5.Task5.V9.Lib
 {
     public class DataService : ISprint5Task5V9
     {
+        private string[] strValues;
+
         public double LoadFromDataFile(string path)
         {
             double res = double.MinValue;
@@ -13,11 +15,18 @@ namespace Tyuiu.KolesnikovMN.Sprint5.Task5.V9.Lib
                 string[] values;
                 while ((line = reader.ReadLine()) != null)
                 {
-                    values = line.Split();
-                    for (int i = 0; i < values.Length; i++)
+                    string[] strValues = line.Split();
+                    double[] dbValues = new double[strValues.Length];
+                    for (int i = 0; i < strValues.Length; i++)
                     {
-                        if (res < double.Parse(values[i], System.Globalization.CultureInfo.InvariantCulture))
-                            res = Math.Round(double.Parse(values[i], System.Globalization.CultureInfo.InvariantCulture), 3);
+                        dbValues[i] = double.Parse(strValues[i], System.Globalization.CultureInfo.InvariantCulture);
+                    }
+                    for (int j = 0;j < dbValues.Length; j++)
+                    {
+                        if (Math.Round(dbValues[j], 0) == dbValues[j])
+                            if (dbValues[j] > res)
+                                res = dbValues[j];
+
                     }
                 }
             }
